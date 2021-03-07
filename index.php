835eb6130f4662e65254ce3cb95bf60c2a159a4a
antesoric <75628780+antesoric@users.noreply.github.com>
2021-03-07 21:30:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ $conn->close(); ?>
     <meta name="author" content="">
     <link rel="icon" href="book.ico">
 
-    <title>DevOps Demo Application build2 </title>
+    <title> 33 DevOps Demo Application build2 </title>
 
     <!-- Bootstrap core CSS -->
     <link href="bootstrap.min.css" rel="stylesheet">
